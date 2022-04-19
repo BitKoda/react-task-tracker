30683f47b892e80cf846af09e4be76725dd90f99
Jamie Finlay <jpfinlay@gmail.com>
2022-04-19 14:46:11 +0100

    Complete app, without routes

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Copyright &copy; 2022 DocMedia</p>
+        {/* <Link to="/about">About</Link> */}
+      
+    </footer>
+  )
+}
+
+export default Footer
